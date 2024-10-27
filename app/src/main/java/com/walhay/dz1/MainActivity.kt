92c192196305f8b95835323ba489f8a4fc67c506
Kolokolov Gleb <kgi23u917@student.bmstu.ru>
2024-10-27 14:55:43 +0300

    Remove not used imports Make squares instead of rects

--- app/src/main/java/com/walhay/dz1/MainActivity.kt
+++ app/src/main/java/com/walhay/dz1/MainActivity.kt
@@ -1,17 +1,8 @@
 package com.walhay.dz1
 
-import android.content.ComponentName
-import android.content.Intent
-import android.content.res.Configuration
 import android.os.Bundle
-import android.widget.LinearLayout
-import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
 
--- app/src/main/java/com/walhay/dz1/NumberTable.kt
+++ app/src/main/java/com/walhay/dz1/NumberTable.kt
@@ -4,9 +4,9 @@ import android.content.res.Configuration
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material3.Button
@@ -20,8 +20,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.unit.FontScaling
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
@@ -38,7 +36,7 @@ fun NumberTable() {
         ){
             items(state) { index ->
                 Box(
-                    modifier = Modifier.size(100.dp)
+                    modifier = Modifier.aspectRatio(1f)
                         .padding(5.dp)
                         .background(if (index % 2 == 0) Color.Red else Color.Blue)
                 )
